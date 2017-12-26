345fffd601a4cce75279f1d603c456418cc54b31
pietahr <pieter.u@hotmail.com>
2017-12-26 13:38:07 +0100

    Initial Commit

---
+++ Gardener/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gardener
+//
+//  Created by Pieter Uyttersprot on 26/12/2017.
+//  Copyright © 2017 Pieter Uyttersprot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
