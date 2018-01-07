a8d637773bfbb853e9a0696f43b1fdfac19ef0d1
pietahr <pieter.u@hotmail.com>
2018-01-07 14:53:30 +0100

    added app icon

--- Gardener/AppDelegate.swift
+++ Gardener/AppDelegate.swift
@@ -1,9 +1,5 @@
-//
-//  AppDelegate.swift
-//  Gardener
-//
-//  Created by Pieter Uyttersprot on 26/12/2017.
-//  Copyright © 2017 Pieter Uyttersprot. All rights reserved.
+//App icon source:
+//<div>Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
 //
 
 import UIKit
